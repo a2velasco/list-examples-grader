0f0ec83694282337303387ea8bb8fd175bddfc65
a2velasco <a2velasco@ucsd.edu>
2024-02-14 15:49:55 -0800

    updated

--- grade.sh
+++ grade.sh
@@ -14,3 +14,36 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "File found."
+else
+    echo "File not found."
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compilation successful."
+else
+    echo "Compilation not successful."
+    exit
+fi 
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-results.txt
+
+grep -h "OK" test-results.txt
+if [[ $? -ne 0 ]]
+then    
+    grep -h "Tests run:" test-results.txt
+    echo "Did not pass all tests. Try again!"
+else
+    echo "Congrats you pass!"
+fi
